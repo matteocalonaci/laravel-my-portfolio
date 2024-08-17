6a0cab6f1c978f44b6ef5a307315f2e8bb08972b
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-08-17 12:40:04 +0200

    php artisan make: model -msr Project

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $project = Project::all();
+        $data =
+        [
+            'project' => $project,
+        ];
+        return view('admin.project.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Project $project)
+    {
+        $data =
+        [
+            'project' => $project,
+        ];
+        return view('admin.project.show', $data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Project $project)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Project $project)
+    {
+        //
+    }
+}
---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $newProject = new Project();
+            $newProject->name = $faker->name();
+            $newProject->image= "https://picsum.photos/id/" . rand(1, 500) . "/800/200";
+            $newProject->description = $faker->text(100);
+            $newProject->github_url = $faker-> text(20);
+            $newProject->save();
+
+    }
+    }
+}
