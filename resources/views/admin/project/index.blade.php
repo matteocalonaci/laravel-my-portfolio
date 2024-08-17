cc18914960d942fb731014d2769e935f3f1e013c
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-08-17 12:42:24 +0200

    Creo le views do project e aggiungo del css

---
+++ resources/views/admin/project/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.admin')
+@section('content')
+<h1 class="text-center p-4">My Projects</h1>
+<div class="card_container">
+    @foreach($project as $progetto)
+            <div class="card">
+                <div class="card-header">
+                    <h5>{{$progetto->name}}</h5>
+                </div>
+                <div class="card-body">
+                    <div class="img-container">
+                        @if (Str::startsWith($progetto->image, "http"))
+
+                        <img src="{{$progetto['image']}}" alt="">
+
+                        @else
+
+                        <img src="{{asset('storage/'.$progetto->image)}}" alt="image">
+                        @endif
+                    </div>
+                    <p>Description: {{$progetto->description}}</p>
+                    <p>Link GitHub: <a href="{{$progetto->github_url}}">{{$progetto->github_url}}</a></p>
+                </div>
+                <div class="card-footer text-center">
+                    <a href="{{ route('admin.project.show', $progetto->id) }}" class="btn btn-primary p-">Vai ai dettagli</a>
+                    <a href="{{ route('admin.project.edit', $progetto->id) }}" class="btn btn-warning p-1">Modifica</a>
+                    {{-- <form action="{{ route('admin.project.destroy', $progetti->id) }}" method="POST
+                        " class="d-inline">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger p-1">Elimina</button>
+                    </form> --}}
+                </div>
+            </div>
+        @endforeach
+</div>
+@endsection
+
+
---
+++ resources/views/admin/project/show.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="card_container">
+        <div class="card-show">
+            <h4 class="mt-4 mb-4">{{ $project['name'] }}</h4>
+            <div class="img_container">
+
+                @if (Str::startsWith($project->image, "http"))
+
+                <img src="{{$project['image']}}" alt="">
+
+                @else
+
+                <img src="{{asset('storage/' . $project->thumb)}}" alt="">
+
+                @endif
+
+            </div>
+        <p class="mt-4">Description: {{$project->description}}</p>
+
+        <p>GitHub: <a href="{{$project->github_url}}">{{$project->github_url}}</a> </p>
+
+
+
+
+
+
+
+
+
+
+
+        {{-- <div class="card-footer text-center">
+                <a href="{{ route('admin.project.show', $project->id) }}" class="btn btn-primary p-">Vai ai dettagli</a>
+                <a href="{{ route('admin.project.edit', $project->id) }}" class="btn btn-warning p-1">Modifica</a>
+                <form action="{{ route('admin.project.destroy', $project->id) }}" method="POST
+                    " class="d-inline">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger p-1">Elimina</button>
+                    </form>
+        </div> --}}
+
+@endsection
