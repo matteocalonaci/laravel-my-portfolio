e74d7372544229c20c73ac8935fee66675285739
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-08-20 15:26:48 +0200

    Modifico il TechnologySeeder e aggiorno il DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Language;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
 
             UserSeeder::class,
+            LanguageSeeder::class,
             TechnologySeeder::class,
             ProjectSeeder::class
 
--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -14,41 +14,30 @@ class TechnologySeeder extends Seeder
     public function run(): void
     {
         $newTechnology = new Technology();
-        $newTechnology->name = 'JavaScript';
-        $newTechnology->description = 'In informatica JavaScript è un linguaggio di programmazione multi paradigma orientato agli eventi, utilizzato sia nella programmazione lato client web che lato server per la creazione di RESTful API, ...';
-        $newTechnology->icon = "fa-brands fa-js";
+        $newTechnology->name = 'Front-end';
+        $newTechnology->description = 'A Front-End Developer is someone who creates websites and web applications. The difference between Front-End and Back-End is that Front-End refers to how a web page looks, while back-end refers to how it works.';
+        $newTechnology->icon ="fa-solid fa-laptop";
         $newTechnology->save();
 
         $newTechnology = new Technology();
-        $newTechnology->name = 'Laravel';
-        $newTechnology->description = 'Laravel è un framework open source di tipo MVC scritto in PHP per lo sviluppo di applicazioni web, creato nel 2011 da Taylor Otwell come derivazione di Symfony.';
-        $newTechnology->icon = "fa-brands fa-laravel";
+        $newTechnology->name = 'Back-end';
+        $newTechnology->description = 'The Back-end developer works "behind the scenes" of a site or web application and takes care of its operation on the data and server side, therefore on what users do not see when visiting the site.';
+        $newTechnology->icon ="fa-solid fa-database";
         $newTechnology->save();
 
         $newTechnology = new Technology();
-        $newTechnology->name = 'PHP';
-        $newTechnology->description = "PHP è un linguaggio di scripting interpretato, originariamente concepito per la programmazione di pagine web dinamiche. L'interprete PHP è un software libero distribuito sotto la licenza PHP.";
-        $newTechnology->icon = "fa-brands fa-php";
+        $newTechnology->name = 'Full stack';
+        $newTechnology->description = "Full-stack developers design and create websites and applications for various platforms. A full-stack developer's job description might include the following: Develop and maintain web services and interfaces. Contribute to front-end and back-end development processes. Build new product features or APIs.";
+        $newTechnology->icon ='fa-solid fa-graduation-cap';
         $newTechnology->save();
 
         $newTechnology = new Technology();
-        $newTechnology->name = 'Vue JS';
-        $newTechnology->description = 'Vue.js è un framework JavaScript open source in configurazione Model–view–viewmodel per la creazione di interfacce utente e single-page applications. È stato creato da Evan You ed è gestito da lui stesso e i membri attivi del core team, provenienti da varie società come Netlify e Netguru. ';
-        $newTechnology->icon = "fa-brands fa-vuejs";
-        $newTechnology->save();
-
-        $newTechnology = new Technology();
-        $newTechnology->name = 'HTML';
-        $newTechnology->description = 'HTML è un acronimo che significa letteralmente "Hyper Text Markup Language", ovvero "Linguaggio a marcatori per ipertesti". Si può spiegare in maniera più semplice: HTML è un linguaggio che permette di impaginare e formattare pagine collegate fra di loro attraverso link - ovvero, i siti web. ';
-        $newTechnology->icon = "fa-brands fa-html5";
+        $newTechnology->name = 'Designer';
+        $newTechnology->description = 'The design engineer, also known as a design engineer, is the professional figure responsible for carrying out complex tasks in the design, development and testing of the physical components of products and systems.';
+        $newTechnology->icon ="fa-solid fa-pen-nib";
         $newTechnology->save();
 
 
-        $newTechnology = new Technology();
-        $newTechnology->name = 'CSS';
-        $newTechnology->description = "CSS, o Cascading Style Sheets, è un linguaggio usato per programmare la resa grafica di documenti scritti in un linguaggio di markup, come HTML e XML. Le classi CSS sono usate per specificare attributi grafici come font, dimensione, colore, spaziatura, bordo e posizione degli elementi all'interno di una rappresentazione web.";
-        $newTechnology->icon = "fa-brands fa-css3";
-        $newTechnology->save();
 
     }
 }
